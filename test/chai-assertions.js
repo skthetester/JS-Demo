56e46d5eab2e93f730681d60c4481866f93dd0e6
skthetester <skthetester@gmail.com>
2022-12-25 13:35:46 -0500

    Add chai assertions

--- test/chai-assertions.js
+++ test/chai-assertions.js
@@ -2,12 +2,29 @@
 
 const chai = require("chai");
 
-describe("Test Suite: Chai Assertions", function(){
-  it("Test Case 01: Expect", function(){
-    expect('apple').to.be.equal('apple', "Expected value matches with Actual");
+//configurations
+chai.config.truncateThreshold = 0; //default = 40
+chai.config.includeStack = true;
+chai.config.showDiff = true;
+
+describe("Test Suite: Chai Assertions", function () {
+  it("Test Case 01: Expect", function () {
+    const expect = chai.expect;
+    expect("apple").to.be.equal(
+      "apple",
+      "Expected value doesn't match with Actual"
+    );
   });
-  
-  it("Test Case 02: Assert", function(){
-    assert.fail("Assertion set to fail");
+
+  it("Test Case 02: Should", function () {
+    const should = chai.should();
+    "apple".should.be.equals(
+      "appel",
+      "Expected value doesn't match with Actual"
+    );
+  });
+
+  it("Test Case 03: Assert", function () {
+    chai.assert.fail("Assertion set to fail");
   });
 });
